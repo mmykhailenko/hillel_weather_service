4c151376c47de60231aab291bb059df349c02434
kate <kate.voskresenska@gmail.com>
2020-08-21 16:14:13 +0300

    add docker-compose-local.yml

--- weather_api/settings.py
+++ weather_api/settings.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = 'weather_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PG_DATABASE_NAME'),
+        'USER': os.environ.get('PG_DATABASE_USER'),
+        'PASSWORD': os.environ.get('PG_DATABASE_PASSWORD'),
+        'HOST': os.environ.get('PG_DATABASE_HOST'),
+        'PORT': os.environ.get('PG_DATABASE_PORT')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
